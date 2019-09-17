8074242015c6fdf84274ed1381db9d6efda0f07b
Nirav Patel <niravdpatel224@gmail.com>
2019-09-17 22:40:57 +0530

    Setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ form.addEventListener('submit', (e)=>{
     const location = search.value;
     result.innerHTML = 'Loading...';
 
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
     response.json().then((data) => {
         if(data.error){
             result.innerHTML = data.error;
